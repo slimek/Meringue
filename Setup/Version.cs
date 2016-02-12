9620cf89e8c1f1457dd9d5acc5982b1070ee0a67
slimek <slime573@gmail.com>
2016-02-12 11:55:44 +0800

    Initial revision.

---
+++ Setup/Version.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Meringue
+{
+
+/// <summary>
+///  Version Number of Meringue Library.
+/// </summary>
+public class Version
+{
+    public static readonly int Major = 0;
+    public static readonly int Minor = 0;
+    public static readonly int Build = 0;
+}
+    
+}  // namespace Meringue
