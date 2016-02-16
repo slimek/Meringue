8c2ea26c7e20c97c4dd69acda9b5de2ce408c96c
slimek <slime573@gmail.com>
2016-02-16 20:53:43 +0800

    [Setup] Rename Version to VersionNumber.

--- Setup/VersionNumber.cs
+++ Setup/VersionNumber.cs
@@ -5,7 +5,7 @@ namespace Meringue
 /// <summary>
 ///  Version Number of Meringue Library.
 /// </summary>
-public class Version
+public class VersionNumber
 {
     public static readonly int Major = 0;
     public static readonly int Minor = 1;
