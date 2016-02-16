cdb84aa35753b2dd7ecbf7f1960647da39c4056e
slimek <slime573@gmail.com>
2016-02-16 21:39:50 +0800

    [Tools] Add ScriptableObjectTool.

---
+++ Editor/Tools/ScriptableObjectTool.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+namespace Meringue
+{
+
+/// <summary>
+/// A tool class to create an asset file under Assets folder.
+/// </summary>
+/// <see cref="http://wiki.unity3d.com/index.php?title=CreateScriptableObjectAsset"/>
+public class ScriptableObjectTool
+{
+    public static void CreateAsset< T >() where T : ScriptableObject
+    {
+        var asset = ScriptableObject.CreateInstance< T >();
+
+        string path = AssetDatabase.
+            GenerateUniqueAssetPath( "Assets/New" + typeof( T ).ToString() + ".asset" );
+
+        AssetDatabase.CreateAsset( asset, path );
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+    }
+}
+
+
+}  // namespace Meringue
