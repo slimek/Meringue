5d61ff6bcd421967fa0ab194fbf0ed7f0ea1d50c
slimek <slime573@gmail.com>
2017-01-16 23:58:59 +0800

    Add Mvp.Presenter

---
+++ Mvp/Presenter.cs
@@ -0,0 +1,37 @@
+﻿#if USE_UNI_RX
+
+using System;
+
+namespace Meringue.Mvp
+{
+    public class Presenter
+    {
+        public static PresenterInterface Create<PresenterInterface>()
+        {
+            var interfaceName = typeof(PresenterInterface).Name;
+
+            if (!interfaceName.StartsWith("I"))
+            {
+                throw new NotSupportedException(
+                    string.Format(
+                        "Presenter interface name must start with \"I\", but given \"{0}\"",
+                        interfaceName
+                ));
+            }
+
+            var className = interfaceName.Substring(1);
+
+            var stubClassName = "Stub" + className;
+            var stubType = Type.GetType(stubClassName);
+            if (stubType != null)
+            {
+                return (PresenterInterface)Activator.CreateInstance(stubType);
+            }
+
+            var type = Type.GetType(className);
+            return (PresenterInterface)Activator.CreateInstance(type);
+        }
+    }
+} // namespace Meringue.Mvp
+
+#endif // USE_UNI_RX
